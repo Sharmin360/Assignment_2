d5f0f5486730321ae13d950628c153de5f91fdd6
Sharmin <info.sharmin360@gmail.com>
2024-09-26 01:31:55 +0600

    Assignment_2

--- app/src/main/java/com/example/myapplication/RadioButtonActivity.java
+++ app/src/main/java/com/example/myapplication/RadioButtonActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RadioButton;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +14,9 @@ public class RadioButtonActivity extends AppCompatActivity {
     private RadioButton radioButtonOption2;
     private RadioButton radioButtonOption3;
 
+    private Button radioshowButton;
+    private TextView radioresultTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +25,22 @@ public class RadioButtonActivity extends AppCompatActivity {
         radioButtonOption1 = findViewById(R.id.radioButtonOption1Id);
         radioButtonOption2 = findViewById(R.id.radioButtonOption2Id);
         radioButtonOption3 = findViewById(R.id.radioButtonOption3Id);
+        radioshowButton = findViewById(R.id.RadioshowButtonId);
+        radioresultTextView = findViewById(R.id.RadioresultTextViewId);
+
+        radioshowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (radioButtonOption1.isChecked()) {
+                    radioresultTextView.setText("Option 1 selected");
+                } else if (radioButtonOption2.isChecked()) {
+                    radioresultTextView.setText("Option 2 selected");
+                } else if (radioButtonOption3.isChecked()) {
+                    radioresultTextView.setText("Option 3 selected");
+                } else {
+                    radioresultTextView.setText("No radio selected");
+                }
+            }
+        });
     }
 }
